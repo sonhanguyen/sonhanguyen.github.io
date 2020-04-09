5cccbe093db89d2143ad4c686cca1a62b94c1e20
Harry <sonha@Harrys-MacBook-Pro.local>
2020-04-09 21:50:17 +1000

    Disable css-loader pure mode

--- app-scripts/webpack/index.js
+++ app-scripts/webpack/index.js
@@ -6,6 +6,7 @@ exports.createPatch = (pattern = require('fs').realpathSync('../..')) => config 
     createPatchWithTsConfig(),
     patchWithAstroturfLoader,
     patchWithWebAssetsLoader,
+    patchCssModuleImpure,
     patchWithMdxLoader,
     createPatchLoadersWithWorkspaceModules(pattern)
   ].reduce(
@@ -14,7 +15,20 @@ exports.createPatch = (pattern = require('fs').realpathSync('../..')) => config 
   )
 
 const createPatchLoadersWithWorkspaceModules = pattern => ({ module: { rules } }) =>
-  rules.forEach(cloned => cloned.include = RegExp(pattern + '(?!.*node_modules.*)'))
+  rules.forEach(rule => rule.include = RegExp(pattern + '(?!.*node_modules.*)'))
+
+const patchCssModuleImpure =  ({ module: { rules } }) => {
+  const cssModuleOptions = rule => {
+    const { options = {} } = rule
+    
+    return options.modules || {}
+  }
+  
+  deepFind(rules, ({ loader, rule }) =>
+    /\/css-loader\//.test(loader) &&
+    cssModuleOptions(rule).mode == 'pure'
+  ).forEach(({ rule }) => cssModuleOptions(rule).mode = 'local')
+}
 
 const patchWithAstroturfLoader = ({ module: { rules } }) =>
   rules.push({
@@ -31,4 +45,25 @@ const patchResoverWithAppsNodeModules = ({ resolve }) => {
   const LIB = 'node_modules'
   const { modules = [ LIB ] } = resolve
   resolve.modules = [ path.join(process.cwd(), LIB), ...modules ]
+}
+
+const deepFind = (current, match, main = {}) => {
+  const {
+    use = current,
+    loader = use,
+    oneOf = loader,
+    loaders = oneOf,
+    rules = loaders,
+    test = main.test,
+  } = current
+
+  if(typeof loader == 'string') return [
+    { rule: current, loader, test: String(test) }
+  ].filter(match)
+  
+  if (Array.isArray(rules)) return [].concat(
+    ...rules.map(rule => deepFind(rule, match, current))
+  )
+  
+  return []
 }
